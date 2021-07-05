b6a7f1406f60c2b6d0149367dbd55d38d0f4f114
LeTiina <tiina.j.le@gmail.com>
2021-07-05 14:50:44 +0300

    added new class person

--- git_pro.py
+++ git_pro.py
@@ -1,2 +1,28 @@
+class Person:
+    name = ""
+    age = 0
+
+    def __init__(self) -> None:
+        self.name = ""
+        self.age = 0
+    
+    def get_name(self):
+        return self.name
+    
+    def get_age(self):
+        return self.age
+    
+    def set_name(self,name):
+        self.name = name
+    
+    def set_age(self,age):
+        self.age = age
+    
+    def __str__(self) -> str:
+        print(self.name,self.age)
+    
+
+        
+
 def hello():
-    return "Hello World!"
+    return "Hello dev!"
